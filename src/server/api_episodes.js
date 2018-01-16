22f8ffe8f0b30a9639e17d1260749be0bff97c22
fldamiens <florent.damiens@etu.udamail.fr>
2018-01-16 17:32:53 +0100

    Modification backend

--- src/server/api_episodes.js
+++ src/server/api_episodes.js
@@ -22,21 +22,13 @@ router.post('/', function(req, res) {
     res.status(400).json({result : "error", message : 'Wrong parameters in the request'})
   }else if(!("name" in req.body) || !("code" in req.body) || !("score" in req.body)){
     res.status(400).json({result : "error", message : 'Wrong parameters keys in the request'})
-<<<<<<< HEAD
   }else if((typeof(req.body["name"]) != 'string' && req.body["name"].length != 0) || (typeof(req.body["code"]) != 'string' && req.body["code"].length != 0) || ((parseFloat(req.body["score"])) != NaN || typeof(parseFloat(req.body["score"])) != "number") ){
-=======
-  }else if((typeof(req.body["name"]) != 'string' && req.body["name"].length != 0) || (typeof(req.body["code"]) != 'string' && req.body["code"].length != 0) || typeof(parseFloat(req.body["score"])) != "number" ){
->>>>>>> b8d69a5db7723f3f1597ffa81d8b9794a21209f6
     res.status(400).json({result : "error", message : 'Illegal value in the request'})
   }else{
     var episode = req.body;
     episode['id'] = //creer ID alea
     //enregistrer dans le fichier
-<<<<<<< HEAD
     res.status(200).json({result : "success", message : "The episode has been created"});
-=======
-    res.send({result : "success", message : "The episode has been created"});
->>>>>>> b8d69a5db7723f3f1597ffa81d8b9794a21209f6
   }
 });
 
